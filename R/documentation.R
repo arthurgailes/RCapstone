65e45617279f023fa9bb768116a360954dbfe953
arthurgailes <agailes1@gmail.com>
2017-12-06 15:30:06 -0800

    Working package

--- R/documentation.R
+++ R/documentation.R
@@ -16,19 +16,19 @@
 #' @importFrom dplyr select
 #' @importFrom tidyr unite
 #' @importFrom lubridate as_date
-#' @importFrom dplyr %>%
+#' @importFrom magrittr "%>%"
+#' @import magrittr
 #'
 #'@export
 eq_clean_data <- function(filename = 'data/signif.txt') {
   
-  table <- suppressMessages(readr::read_tsv(filename, progress = F)) %>%
-    dplyr::filter(YEAR >=2000, is.na(LONGITUDE) ==F, is.na(LATITUDE) == F) %>%
-    dplyr::select(I_D, YEAR, MONTH, DAY, LONGITUDE, LATITUDE, 
+  table <- suppressMessages(readr::read_tsv(filename, progress = F)) 
+  table <- dplyr::filter(table, YEAR >=2000, is.na(LONGITUDE) ==F, is.na(LATITUDE) == F) 
+  table <- dplyr::select(table, I_D, YEAR, MONTH, DAY, LONGITUDE, LATITUDE, 
                   Richter_Magnitude = EQ_MAG_ML, DEATHS, Magnitude = EQ_PRIMARY) 
   
   table[is.na(table)] <- 01
-  table <- table %>%
-    tidyr::unite(col = "Date", from = c(YEAR, MONTH, DAY), sep = "-") 
+  table <- tidyr::unite(table, col = "Date", from = c(YEAR, MONTH, DAY), sep = "-") 
   
   table$Date <- lubridate::as_date(table$Date)
   #class(table$LATITUDE) <- "numeric"
@@ -54,18 +54,18 @@ eq_clean_data <- function(filename = 'data/signif.txt') {
 #' @importFrom dplyr select
 #' @importFrom dplyr mutate
 #' @importFrom dplyr mutate_all
-#' @importFrom dplyr %>%
+#' @importFrom magrittr "%>%"
 #' 
 #' @export
 eq_location_clean <- function(filename = 'data/signif.txt') {
-  loc <- suppressMessages(readr::read_tsv(filename, progress = F)) %>%
-    dplyr::select(I_D, COUNTRY, LOCATION_NAME) %>% 
-    tidyr::separate(LOCATION_NAME, into = c('delete', 'City'), sep = ': ', 
-                    extra = 'merge', fill = 'right') %>%
-    dplyr::select(-delete) %>% 
-    dplyr::mutate_all(.funs = stringr::str_to_title) %>% 
-    dplyr::mutate(I_D = as.numeric(I_D))
-    
+  loc <- suppressMessages(readr::read_tsv(filename, progress = F)) 
+  loc <- dplyr::select(loc, I_D, COUNTRY, LOCATION_NAME) 
+  loc <- tidyr::separate(loc, LOCATION_NAME, into = c('delete', 'City'), sep = ': ', 
+                    extra = 'merge', fill = 'right') 
+  loc <- dplyr::select(loc, -delete) 
+  loc <- dplyr::mutate_all(loc, .funs = stringr::str_to_title) 
+  loc <- dplyr::mutate(loc, I_D = as.numeric(I_D))
+  loc
 }
 
 #' Graph the timeline
@@ -77,6 +77,7 @@ eq_location_clean <- function(filename = 'data/signif.txt') {
 #' 
 #' @importFrom dplyr left_join
 #' @importFrom dplyr filter
+#' @importFrom magrittr "%>%" 
 #' @import ggplot2
 #' 
 #' @examples
@@ -84,22 +85,24 @@ eq_location_clean <- function(filename = 'data/signif.txt') {
 #' 
 #' @export
 geom_timeline <- function() {
-  graph_data <- eq_clean_data() %>% 
-    dplyr::left_join(eq_location_clean(), by = 'I_D') %>%
-    dplyr::filter(COUNTRY == 'China' | COUNTRY == 'Usa')
+  graph_data <- eq_clean_data() 
+  graph_data <- dplyr::left_join(graph_data, eq_location_clean(), by = 'I_D') 
+  graph_data <- dplyr::filter(graph_data, COUNTRY == 'China' | COUNTRY == 'Usa')
   
-  ggplot2::ggplot(graph_data, aes(x = Date, y = COUNTRY)) +
-  geom_line() +
-  geom_point(aes(size = Richter_Magnitude, fill = DEATHS), alpha = 0.1) +
-  scale_y_discrete(breaks = c('Usa', 'China'), labels = c('USA', 'China'))+
-  scale_fill_gradient(limits = c(0,10), breaks = c(0,5,10)) +
-  scale_size_continuous(breaks = c(2,4,6)) +
-  labs(fill = '# deaths', size = 'Richter scale value') +
-  theme(legend.position = 'bottom', panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), panel.background = element_blank(),
-        axis.line.x = element_line(colour = 'black'),
-        axis.title.y=element_blank(),
-        axis.ticks.y=element_blank())
+  ggplot2::ggplot(graph_data, ggplot2::aes(x = Date, y = COUNTRY)) +
+    ggplot2::geom_line() +
+    ggplot2::geom_point(ggplot2::aes(size = Richter_Magnitude, fill = DEATHS), 
+                        alpha = 0.1) +
+    ggplot2::scale_y_discrete(breaks = c('Usa', 'China'), 
+                              labels = c('USA', 'China'))+
+    ggplot2::scale_fill_gradient(limits = c(0,10), breaks = c(0,5,10)) +
+    ggplot2::scale_size_continuous(breaks = c(2,4,6)) +
+    ggplot2::labs(fill = '# deaths', size = 'Richter scale value') +
+    ggplot2::theme(legend.position = 'bottom', panel.grid.major = ggplot2::element_blank(),
+        panel.grid.minor = ggplot2::element_blank(), panel.background = ggplot2::element_blank(),
+        axis.line.x = ggplot2::element_line(colour = 'black'),
+        axis.title.y=ggplot2::element_blank(),
+        axis.ticks.y=ggplot2::element_blank())
 }
 
 #' Graph the earthquake timeline for the US and China, with high magnitudes noted.
@@ -113,7 +116,7 @@ geom_timeline <- function() {
 #' @export
 geom_timeline_label <- function(){
   geom_timeline +
-  geom_text(aes(label = ifelse(graph_data$Richter_Magnitude >5, 
+    ggplot2::geom_text(ggplot2::aes(label = ifelse(graph_data$Richter_Magnitude >5, 
                                as.character(graph_data$City),'')),
             angle = 45, hjust = 'bottom', nudge_y = 0.1, size = 2) 
 }
@@ -126,11 +129,13 @@ geom_timeline_label <- function(){
 #' @examples 
 #' eq_map('popup_text')
 eq_create_label <- function() {
-  
+  orig <- eq_clean_data() 
+  orig <- dplyr::left_join(orig, eq_location_clean(), by = 'I_D')
+  orig <- dplyr::filter(orig, COUNTRY == 'Mexico') 
   paste('<b>Location: </b>', 
-        as.character(map_data$City), '<br />', '<b>Magnitude: </b>', 
-        as.character(map_data$Magnitude), '<br />', '<b>Total deaths: </b>', 
-        as.character(map_data$DEATHS))
+        as.character(orig$City), '<br />', '<b>Magnitude: </b>', 
+        as.character(orig$Magnitude), '<br />', '<b>Total deaths: </b>', 
+        as.character(orig$DEATHS))
 }
 
 
@@ -142,6 +147,7 @@ eq_create_label <- function() {
 #' @importFrom dplyr left_join
 #' @importFrom dplyr filter
 #' @importFrom dplyr mutate
+#' @importFrom magrittr "%>%" 
 #' @importFrom leaflet leaflet
 #' @importFrom leaflet addTiles
 #' @importFrom leaflet addCircleMarkers
@@ -149,17 +155,19 @@ eq_create_label <- function() {
 #' @examples 
 #' eq_map('Date')
 eq_map <- function(annot_col = 'popup_text') {
-  map_data <- eq_clean_data() %>% 
-    dplyr::left_join(eq_location_clean(file), by = 'I_D') %>%
-    dplyr::filter(COUNTRY == 'Mexico') %>%
-    dplyr::mutate(popup_text = eq_create_label())
+  map_data <- eq_clean_data() 
+  map_data <- dplyr::left_join(map_data, eq_location_clean(), by = 'I_D')
+  map_data <- dplyr::filter(map_data, COUNTRY == 'Mexico') 
+  map_data <- dplyr::mutate(map_data, popup_text = eq_create_label())
   
-  leaflet::leaflet() %>% 
-    leaflet::addTiles() %>% 
-    leaflet::addCircleMarkers(data = map_data, lng = ~ LONGITUDE, 
+  map <- leaflet::leaflet() 
+  map <- leaflet::addTiles(map) 
+  map <- leaflet::addCircleMarkers(map, data = map_data, lng = ~ LONGITUDE, 
                               lat = ~ LATITUDE, opacity = 0.3, color = '#03F',
-                              radius = map_data$Magnitude, 
+                              radius = map_data$Magnitude*1.5, 
                               fillOpacity = 0.3, weight = 1, 
                               popup = (as.character(map_data[[annot_col]])))
+  map
 }
+
 
